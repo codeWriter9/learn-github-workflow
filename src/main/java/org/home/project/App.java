41503a331d3571a5301bd4f31f5b185eefe1a5db
Sanjay Ghosh <nerdsubhankar2006@gmail.com>
2025-03-21 20:28:24 +0530

    Fix Spring Issues

--- src/main/java/org/home/project/App.java
+++ src/main/java/org/home/project/App.java
@@ -1,15 +1,21 @@
+package org.home.project;
+
+import lombok.extern.slf4j.Slf4j;
+
+
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
-public final class App implements CommandLineRunner {
+@Slf4j
+public class App implements CommandLineRunner {
 
 	public static void main(String [] args) {
 		SpringApplication.run(App.class, args);
 	}
 
 	public void run(String [] args) {
-		System.out.println("Hello World!");
+		log.info("Hello World!");
 	}
 
 
